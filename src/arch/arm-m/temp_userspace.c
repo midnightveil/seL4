4a2b0272667ce10a9f611b2959f1def87007dd7b
julia <midnight@trainwit.ch>
2025-12-06 17:22:53 +1100

    fix sections
    Signed-off-by: julia <midnight@trainwit.ch>

--- src/arch/arm-m/temp_userspace.c
+++ src/arch/arm-m/temp_userspace.c
@@ -10,7 +10,7 @@
 void _user_main(void) SECTION(".user.main");
 
 char _user_stack[0x400] SECTION(".user.stack");
-word_t _user_stack_top = (word_t)&_user_stack[0x400 - 2 * sizeof(word_t)];
+word_t _user_stack_top SECTION(".user.stack") = (word_t)&_user_stack[0x400 - 2 * sizeof(word_t)];
 
 void _user_main(void) {
     for (int v = 0; v < 10; v++) {
