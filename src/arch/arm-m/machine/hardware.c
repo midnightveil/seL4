64518ed258f9e59d2de6f626465c4e42dabdcd64
julia <midnight@trainwit.ch>
2026-04-05 15:27:32 +1000

    setNextSP should decrease the SP by framesize
    Signed-off-by: julia <midnight@trainwit.ch>

--- src/arch/arm-m/machine/hardware.c
+++ src/arch/arm-m/machine/hardware.c
@@ -15,5 +15,15 @@ void setNextPC(tcb_t *thread, word_t v)
 
 void setNextSP(tcb_t *thread, word_t v)
 {
-    setRegister(thread, SP_process, v);
+    /**
+     * We assume that in this situation when we set a SP explicitly we are
+     * setting the top of the stack (this is the only user of this function
+     * at the moment). Hence, since Cortex-M does hardware (un)stacking of
+     * the exception frame, to simulate a thread that is ready for unstacking
+     * (i.e. in the kernel) we must 'push' the stack pointer downwards by
+     * the size of the exception stack frame.
+     *
+     * We have disabled the FPU, so framesize = 0x20, which is the value of PT_R4.
+     **/
+    setRegister(thread, SP_process, v - PT_R4);
 }
